79393fc343f35b642a094b15e2298286098bfc16
Jaume Singla <jaume.singlavalls@gmail.com>
2015-06-09 15:23:22 +0200

    Changed the code of group route, to load all pages of the root level

--- app/routes/group.js
+++ app/routes/group.js
@@ -1,7 +1,30 @@
 import ProtectedRoute from './protected';
+import Ember from 'ember';
 
 export default ProtectedRoute.extend({
 	model: function (){
-		return this.store.find('group', {parent:null});
+		return new Ember.RSVP.Promise(function (resolve, reject){
+			var data;
+			var totalData=[];
+			var callPage=function (page){
+				return this.store.find('group', {parent: null, page:page});
+			}.bind(this);
+			var resolveCallback=function (d){
+				totalData=totalData.concat(d.get('content'));
+				var next=data.get('content.meta.next');
+				if (next!==null){
+					data=callPage(next);
+					data.then(resolveCallback, function (e){
+						reject(e);
+					})
+				} else {
+					resolve(totalData);
+				}
+			};
+			data=callPage(1);
+			data.then(resolveCallback, function (e){
+				reject(e);
+			});
+		}.bind(this));
 	}
 });
